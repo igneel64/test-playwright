b90d5e55f2073b6d44c6b9ce1ce1674250101489
Peter Perlepes <p.perlepes@gmail.com>
2022-06-23 01:18:59 +0300

    Tracer

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,10 @@
 import { test, expect } from "@playwright/test";
 
-test.use({ headless: false, storageState: "state.json" });
+test.use({ headless: false });
 
 test.describe("Getting started!", () => {
   test("should check the page title", async ({ page }) => {
     await page.goto("https://cosmossport.gr");
-    // await page.evaluate(() =>
-    //   document.querySelector("#search_query_top")?.remove()
-    // );
-    await expect(page).toHaveScreenshot();
+    expect.soft(await page.title()).toBe("Cosmossport");
   });
 });
