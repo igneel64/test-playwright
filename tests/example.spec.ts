eef41959a75c5df0c32fce393f349e35b050cfa2
Peter Perlepes <p.perlepes@gmail.com>
2022-06-23 01:04:53 +0300

    Block and state

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,11 +1,17 @@
 import { test, expect } from "@playwright/test";
 
-test.use({ headless: false });
+test.use({ headless: false, storageState: "state.json" });
 
 test.describe("Getting started!", () => {
-  test("should check the page title", async ({ page }) => {
+  test("should check the page title", async ({ page, context }) => {
+    await page.route(
+      (url: URL) => Boolean(url.href.match("sentry")),
+      (route) => route.abort()
+    );
+
     await page.goto("https://cosmossport.gr");
     expect.soft(await page.title()).toBe("Cosmossport");
     await page.pause();
+    // await context.storageState({ path: "state.json" });
   });
 });
