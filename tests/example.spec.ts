58027284af03d99f8da6a19465a381fc97020d33
Peter Perlepes <p.perlepes@gmail.com>
2022-06-23 01:10:12 +0300

    Screenshot diff

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,15 +3,11 @@ import { test, expect } from "@playwright/test";
 test.use({ headless: false, storageState: "state.json" });
 
 test.describe("Getting started!", () => {
-  test("should check the page title", async ({ page, context }) => {
-    await page.route(
-      (url: URL) => Boolean(url.href.match("sentry")),
-      (route) => route.abort()
-    );
-
+  test("should check the page title", async ({ page }) => {
     await page.goto("https://cosmossport.gr");
-    expect.soft(await page.title()).toBe("Cosmossport");
-    await page.pause();
-    // await context.storageState({ path: "state.json" });
+    // await page.evaluate(() =>
+    //   document.querySelector("#search_query_top")?.remove()
+    // );
+    await expect(page).toHaveScreenshot();
   });
 });
